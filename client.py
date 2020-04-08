b7b4451e2b7f892cc5a7f7e37ae2ab545f633ba6
Primdahl26 <frederik.vincent@hotmail.com>
2020-04-08 21:23:40 +0200

    Fixed some bugs

--- client.py
+++ client.py
@@ -7,32 +7,20 @@ server_address = ('localhost', 10000)
 message = 'Message from client'
 
 hostname = socket.gethostname()
-ipAddress = socket.gethostbyname(hostname).encode()
+ipAddress = socket.gethostbyname(hostname)
 
 incrementingNumber = 0
 
 try:
     # Send ip address to get accepted
     print('Sending IP...\nSending: {!r}'.format(ipAddress))
-    sent = sock.sendto(ipAddress, server_address)
+    sent = sock.sendto('com-0 '.encode()+ipAddress.encode(), server_address)
 
     while True:
         print('\nWaiting to receive...')
         data, server = sock.recvfrom(4096)
         print('Received {} bytes from {}'.format(len(data), server))
         print('Data: ' + str(data))
-
-        if data[:3] == b'com':
-            acceptMessage = 'com-' + str(incrementingNumber) + ' accept'
-
-            print('Sending: ' + acceptMessage)
-            sent = sock.sendto(acceptMessage.encode(), server_address)
-
-            incrementingNumber += 2
-            firstMessage = 'msg-' + str(incrementingNumber) + '=Hello, i am new user'
-
-            print('Sending: ' + firstMessage)
-            sent = sock.sendto(firstMessage.encode(), server_address)
 
         if data[:3] == b'res':
             incrementingNumber += 2
@@ -41,6 +29,16 @@ try:
             print('Sending: ' + okMessage)
             sock.sendto(okMessage.encode(), server_address)
 
+        if data[:12] == b'com-0 accept':
+            acceptMessage = 'com-' + str(incrementingNumber) + ' accept'
+
+            print('Sending: ' + acceptMessage)
+            sent = sock.sendto(acceptMessage.encode(), server_address)
+
+            firstMessage = 'msg-' + str(incrementingNumber) + '=Hello, i am new user'
+
+            print('Sending: ' + firstMessage)
+            sent = sock.sendto(firstMessage.encode(), server_address)
 
 finally:
     print('\nReached finally block\nClosing socket...')
--- server.py
+++ server.py
@@ -11,21 +11,27 @@ sock.bind(server_address)
 incrementingNumber = 1
 
 while True:
-    print('\nWaiting to receive message...')
+    print('\nWaiting to receive...')
     data, address = sock.recvfrom(4096)
 
     print('Received {} bytes from {}'.format(len(data), address))
     print('Data: '+str(data))
 
-    if data[:3] == b'192':
+    if data[:7] == b'com-0 1':
         acceptMessage = 'com-0 accept ' + socket.gethostbyname(socket.gethostname())
 
         sent = sock.sendto(acceptMessage.encode(), address)
         print('Sending: '+str(acceptMessage))
 
+        clientDataIP = data.split()[-1]
+
     if data[:3] == b'msg':
-        incrementingNumber += 2
         automatedMessage = 'res-' + str(incrementingNumber) + '=I am server'
+        incrementingNumber += 2
 
         print('Sending: '+automatedMessage)
         sent = sock.sendto(automatedMessage.encode(), address)
+
+    if data[:12] == b'com-0 accept':
+
+        print('\nThe current active user is: ' + str(clientDataIP))
