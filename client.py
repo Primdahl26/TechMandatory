b74a132ac26b603ed2534f5c9236e99bde7b78ec
Primdahl26 <frederik.vincent@hotmail.com>
2020-04-16 15:42:47 +0200

    Added tolerance

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import  time
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,37 +10,55 @@ message = 'Message from client'
 hostname = socket.gethostname()
 ipAddress = socket.gethostbyname(hostname)
 
-incrementingNumber = 0
 
-try:
-    # Send ip address to get accepted
-    print('Sending IP...\nSending: {!r}'.format(ipAddress))
-    sent = sock.sendto('com-0 '.encode()+ipAddress.encode(), server_address)
+def main():
+    incrementingNumber = 0
 
-    while True:
-        print('\nWaiting to receive...')
-        data, server = sock.recvfrom(4096)
-        print('Received {} bytes from {}'.format(len(data), server))
-        print('Data: ' + str(data))
+    try:
+        # Send ip address to get accepted
+        print('Sending IP...\nSending: {!r}'.format(ipAddress))
+        sent = sock.sendto('com-0 '.encode() + ipAddress.encode(), server_address)
 
-        if data[:3] == b'res':
-            incrementingNumber += 2
-            okMessage = 'msg-' + str(incrementingNumber) + '=Ok, good to know'
+        while True:
+            # Start up the client socket, and wait for a message
+            print('\nWaiting to receive...')
+            data, server = sock.recvfrom(4096)
+            print('Received {} bytes from {}'.format(len(data), server))
+            print('Data: ' + str(data))
 
-            print('Sending: ' + okMessage)
-            sock.sendto(okMessage.encode(), server_address)
+            # If a normal message is received
+            if data[:3] == b'res':
+                incrementingNumber += 2
+                okMessage = 'msg-' + str(incrementingNumber) + '=Ok, good to know'
 
-        if data[:12] == b'com-0 accept':
-            acceptMessage = 'com-' + str(incrementingNumber) + ' accept'
+                print('Sending: ' + okMessage)
+                sock.sendto(okMessage.encode(), server_address)
 
-            print('Sending: ' + acceptMessage)
-            sent = sock.sendto(acceptMessage.encode(), server_address)
+                time.sleep(8)
 
-            firstMessage = 'msg-' + str(incrementingNumber) + '=Hello, i am new user'
+            # If accept message is received
+            if data[:12] == b'com-0 accept':
+                acceptMessage = 'com-' + str(incrementingNumber) + ' accept'
 
-            print('Sending: ' + firstMessage)
-            sent = sock.sendto(firstMessage.encode(), server_address)
+                print('Sending: ' + acceptMessage)
+                sent = sock.sendto(acceptMessage.encode(), server_address)
 
-finally:
-    print('\nReached finally block\nClosing socket...')
-    sock.close()
+                firstMessage = 'msg-' + str(incrementingNumber) + '=Hello, i am new user'
+
+                print('Sending: ' + firstMessage)
+                sent = sock.sendto(firstMessage.encode(), server_address)
+
+            # If it is asked to shut down
+            if data[:12] == b'con-res 0xFE':
+                closeMessage = 'con-res 0xFF'
+                print('\nIdle for more than 4 seconds\nClosing down connection with following message: ' + closeMessage)
+
+                sent = sock.sendto(closeMessage.encode(), server_address)
+                break
+
+    finally:
+        print('\nReached finally block\nClosing socket...')
+        sock.close()
+
+
+main()
--- server.py
+++ server.py
@@ -1,37 +1,80 @@
 import socket
+import time
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
+# Startup the Servers socket
 server_address = ('localhost', 10000)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
-incrementingNumber = 1
 
-while True:
-    print('\nWaiting to receive...')
-    data, address = sock.recvfrom(4096)
+def main():
+    # A number that we add up to see the message number
+    incrementingNumber = 1
 
-    print('Received {} bytes from {}'.format(len(data), address))
-    print('Data: '+str(data))
+    # Make sure that the time if statement runs only once
+    hasBeenRun = False
 
-    if data[:7] == b'com-0 1':
-        acceptMessage = 'com-0 accept ' + socket.gethostbyname(socket.gethostname())
+    # A number that keeps track of how many packages was received
+    packageCounter = 0
 
-        sent = sock.sendto(acceptMessage.encode(), address)
-        print('Sending: '+str(acceptMessage))
+    # Time to handle the tolerance
+    currentTime = time.time()
+    timeoutTime = currentTime + 4
+    total_seconds = round(currentTime - timeoutTime).__str__()
 
-        clientDataIP = data.split()[-1]
+    while True:
+        # Wait for message from the Client
+        print('\nWaiting to receive...')
 
-    if data[:3] == b'msg':
-        automatedMessage = 'res-' + str(incrementingNumber) + '=I am server'
-        incrementingNumber += 2
+        data, address = sock.recvfrom(4096)
 
-        print('Sending: '+automatedMessage)
-        sent = sock.sendto(automatedMessage.encode(), address)
+        if hasBeenRun is False:
+            print('Time until timeout: ' + str(total_seconds[1:])+' seconds')
 
-    if data[:12] == b'com-0 accept':
+        print('Received {} bytes from {}'.format(len(data), address))
+        print('Data: ' + str(data))
 
-        print('\nThe current active user is: ' + str(clientDataIP))
+        # If the IP is received
+        if data[:7] == b'com-0 1':
+            acceptMessage = 'com-0 accept ' + socket.gethostbyname(socket.gethostname())
+
+            sent = sock.sendto(acceptMessage.encode(), address)
+            print('Sending: ' + str(acceptMessage))
+
+            clientDataIP = data.split()[-1]
+
+        # If a message is received
+        if data[:3] == b'msg':
+            timeoutTime = currentTime + 4
+
+            automatedMessage = 'res-' + str(incrementingNumber) + '=I am server'
+            incrementingNumber += 2
+
+            print('Sending: ' + automatedMessage)
+            sent = sock.sendto(automatedMessage.encode(), address)
+
+        # If an accept message is received=
+        if data[:12] == b'com-0 accept':
+            print('\nThe current active user is: ' + str(clientDataIP))
+
+        # If time is greater than 4 seconds
+        if time.time() > timeoutTime and hasBeenRun is False:
+            timeoutMessage = 'con-res 0xFE'
+            print('\n' + clientDataIP.decode() + ' Has been idle for more than 4 seconds\n'
+                                                 'Timing out connection and sending: ' + timeoutMessage)
+
+            sent = sock.sendto(timeoutMessage.encode(), address)
+
+            hasBeenRun = True
+
+        if data[:12] == b'con-res 0xFF':
+
+            print('\nRestarting server...')
+            main()
+
+
+main()
