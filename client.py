58c515bde694df426ecb6c325634de50eda02e8a
Primdahl26 <frederik.vincent@hotmail.com>
2020-04-27 19:48:01 +0200

    Fixed tolerance Put things into Defs Made program run in threads Added opt.conf Added KeepALive that looks in opt.conf for what to do Added a way to make the inputs of the client yourself

--- client.py
+++ client.py
@@ -1,64 +1,120 @@
+import os
 import socket
-import  time
+import threading
+from configparser import ConfigParser
+import time
+import sys
+
+conf = ConfigParser()
+conf.read("opt.conf", encoding='utf-8')
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server_address = ('localhost', 10000)
-message = 'Message from client'
 
 hostname = socket.gethostname()
 ipAddress = socket.gethostbyname(hostname)
+selfWrittenMessage = ''
+
+data = b''
+hasBeenRun = False
 
 
 def main():
     incrementingNumber = 0
+    global hasBeenRun
 
     try:
-        # Send ip address to get accepted
-        print('Sending IP...\nSending: {!r}'.format(ipAddress))
-        sent = sock.sendto('com-0 '.encode() + ipAddress.encode(), server_address)
-
         while True:
-            # Start up the client socket, and wait for a message
-            print('\nWaiting to receive...')
-            data, server = sock.recvfrom(4096)
-            print('Received {} bytes from {}'.format(len(data), server))
-            print('Data: ' + str(data))
-
             # If a normal message is received
-            if data[:3] == b'res':
+            if get_data()[:3] == b'res' and hasBeenRun is False:
                 incrementingNumber += 2
                 okMessage = 'msg-' + str(incrementingNumber) + '=Ok, good to know'
+                print('Enter message: ')
 
-                print('Sending: ' + okMessage)
-                sock.sendto(okMessage.encode(), server_address)
+                write()
 
-                time.sleep(8)
+                finalMessage = 'msg-' + str(incrementingNumber) + '=' + get_self_written_message()
+
+                print('Sending: ' + finalMessage)
+                sock.sendto(finalMessage.encode(), server_address)
+
+                hasBeenRun = True
 
             # If accept message is received
-            if data[:12] == b'com-0 accept':
+            if get_data()[:12] == b'com-0 accept' and hasBeenRun is False:
                 acceptMessage = 'com-' + str(incrementingNumber) + ' accept'
 
                 print('Sending: ' + acceptMessage)
-                sent = sock.sendto(acceptMessage.encode(), server_address)
+                sock.sendto(acceptMessage.encode(), server_address)
 
                 firstMessage = 'msg-' + str(incrementingNumber) + '=Hello, i am new user'
 
                 print('Sending: ' + firstMessage)
-                sent = sock.sendto(firstMessage.encode(), server_address)
+                sock.sendto(firstMessage.encode(), server_address)
 
-            # If it is asked to shut down
-            if data[:12] == b'con-res 0xFE':
-                closeMessage = 'con-res 0xFF'
-                print('\nIdle for more than 4 seconds\nClosing down connection with following message: ' + closeMessage)
-
-                sent = sock.sendto(closeMessage.encode(), server_address)
-                break
+                hasBeenRun = True
 
     finally:
         print('\nReached finally block\nClosing socket...')
         sock.close()
 
 
-main()
+def read():
+    global data, server, hasBeenRun
+    # Send ip address to get accepted
+    print('Sending IP...\nSending: {!r}'.format(ipAddress))
+    sock.sendto('com-0 '.encode() + ipAddress.encode(), server_address)
+    while True:
+        print()
+        # Start up the client socket, and wait for a message
+        data, server = sock.recvfrom(4096)
+        print('Received {} bytes from {}'.format(len(data), server))
+        print('Data: ' + str(data))
+
+        hasBeenRun = False
+
+
+def write():
+    global selfWrittenMessage
+    selfWrittenMessage = input()
+
+
+def check_heartbeat():
+    while True:
+        if get_data()[:12] == b'con-res 0xFE':
+            closeMessage = 'con-res 0xFF'
+
+            sock.sendto(closeMessage.encode(), server_address)
+            print('Idle for more than 4 seconds\nClosing down connection with following message: ' + closeMessage)
+
+            sys.stdout.flush()
+            os._exit(0)
+
+
+def keep_alive():
+    heartbeatMessage = b'con-h 0x00'
+    try:
+        if conf.getboolean("settings", "KeepALive"):
+            while True:
+                time.sleep(3)
+                print('Sending heartbeat to server\nSending: ' + str(heartbeatMessage)+'\n')
+                sock.sendto(heartbeatMessage, server_address)
+
+    finally:
+        print('Something went wrong')
+
+
+def get_self_written_message():
+    return selfWrittenMessage
+
+
+def get_data():
+    return data
+
+
+if __name__ == '__main__':
+    threading.Thread(target=read).start()
+    threading.Thread(target=main).start()
+    threading.Thread(target=check_heartbeat).start()
+    threading.Thread(target=keep_alive).start()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
-import time
+from threading import Timer
+import threading
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,71 +11,134 @@ server_address = ('localhost', 10000)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
+data = b'ignoreThisLongStringNamePlease:)'
+clientDataIP = 123
+hasBeenRun = False
+
+
+def read():
+    global data, hasBeenRun, address
+
+    print('\nServer startup complete\n')
+    while True:
+        print()
+        # Wait for message from the Client
+        data, address = sock.recvfrom(4096)
+
+        timeThread.stop()
+
+        # Print the message from the client
+        print('Received {} bytes from {}'.format(len(data), address))
+        print('Data: ' + str(data))
+
+        hasBeenRun = False
+
 
 def main():
     # A number that we add up to see the message number
     incrementingNumber = 1
-
-    # Make sure that the time if statement runs only once
-    hasBeenRun = False
-
-    # A number that keeps track of how many packages was received
-    packageCounter = 0
-
-    # Time to handle the tolerance
-    currentTime = time.time()
-    timeoutTime = currentTime + 4
-    total_seconds = round(currentTime - timeoutTime).__str__()
+    global hasBeenRun
 
     while True:
-        # Wait for message from the Client
-        print('\nWaiting to receive...')
-
-        data, address = sock.recvfrom(4096)
-
-        if hasBeenRun is False:
-            print('Time until timeout: ' + str(total_seconds[1:])+' seconds')
-
-        print('Received {} bytes from {}'.format(len(data), address))
-        print('Data: ' + str(data))
-
         # If the IP is received
-        if data[:7] == b'com-0 1':
+        if get_data()[:7] == b'com-0 1' and get_has_been_run() is False:
+            incrementingNumber = 1
             acceptMessage = 'com-0 accept ' + socket.gethostbyname(socket.gethostname())
 
-            sent = sock.sendto(acceptMessage.encode(), address)
+            sock.sendto(acceptMessage.encode(), address)
             print('Sending: ' + str(acceptMessage))
 
+            global clientDataIP
             clientDataIP = data.split()[-1]
 
-        # If a message is received
-        if data[:3] == b'msg':
-            timeoutTime = currentTime + 4
+            hasBeenRun = True
+            timeThread.start()
 
+        # If an accept message is received
+        if get_data()[:12] == b'com-0 accept' and get_has_been_run() is False:
+            print('\nThe current active user is: ' + str(clientDataIP))
+
+            hasBeenRun = True
+            timeThread.start()
+
+        # If a message is received
+        if get_data()[:3] == b'msg' and get_has_been_run() is False:
             automatedMessage = 'res-' + str(incrementingNumber) + '=I am server'
             incrementingNumber += 2
 
             print('Sending: ' + automatedMessage)
-            sent = sock.sendto(automatedMessage.encode(), address)
+            sock.sendto(automatedMessage.encode(), address)
 
-        # If an accept message is received=
-        if data[:12] == b'com-0 accept':
-            print('\nThe current active user is: ' + str(clientDataIP))
+            hasBeenRun = True
+            timeThread.start()
 
-        # If time is greater than 4 seconds
-        if time.time() > timeoutTime and hasBeenRun is False:
-            timeoutMessage = 'con-res 0xFE'
-            print('\n' + clientDataIP.decode() + ' Has been idle for more than 4 seconds\n'
-                                                 'Timing out connection and sending: ' + timeoutMessage)
-
-            sent = sock.sendto(timeoutMessage.encode(), address)
+        if get_data()[:12] == b'con-res 0xFF' and get_has_been_run() is False:
+            print('Client disconnected successfully')
 
             hasBeenRun = True
 
-        if data[:12] == b'con-res 0xFF':
+        if get_data() == b'ignoreThisLongStringNamePlease:)':
+            timeThread.stop()
 
-            print('\nRestarting server...')
-            main()
+            hasBeenRun = True
+
+        if get_data() == b'con-h 0x00' and get_has_been_run() is False:
+            timeThread.stop()
+
+            print('Client still alive')
+
+            hasBeenRun = True
 
 
-main()
+def kill_client():
+    timeoutMessage = 'con-res 0xFE'
+    print(str(clientDataIP) + ' Has been idle for more than 4 seconds\n'
+                              'Timing out connection and sending: ' + timeoutMessage + '\n')
+
+    sock.sendto(timeoutMessage.encode(), address)
+
+    timeThread.stop()
+
+
+def get_data():
+    return data
+
+
+def get_address():
+    return address
+
+
+def get_has_been_run():
+    return hasBeenRun
+
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+
+if __name__ == '__main__':
+    timeThread = RepeatedTimer(4, kill_client)
+    threading.Thread(target=read).start()
+    threading.Thread(target=main).start()
